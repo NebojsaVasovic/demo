9fb1f23eaf2fd2a4b8ab8ce6b8cb26ceece03ced
NebojsaVasovic <nebojsa.vasovic.sg0@gmail.com>
2021-04-07 01:28:49 +0200

    ss

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    private final String name;
+    private final String lastName;
+
+    public Test(String name, String lastName){
+        this.lastName = lastName;
+        this.name = name;
+    }
+}
